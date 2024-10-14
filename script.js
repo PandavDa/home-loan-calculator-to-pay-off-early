a47e6ecf0b8f5f4e4191021e50c9a98295f13452
PandavDa <dapandav50@gmail.com>
2024-10-15 00:41:25 +0530

    script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function calculatePayoff() {
   const loanTerm = parseFloat(document.getElementById('loan-term').value);
   const currentMonthlyPayment = parseFloat(document.getElementById('current-monthly-payment').value);
   const payoffGoal = parseFloat(document.getElementById('payoff-goal').value);
-  const extraPayment = parseFloat(document.getElementById('extra-payment-amount').value) || 0;
+  const extraPayment = parseFloat(document.getElementById('extra-payment-amount').value);
 
   const totalInterestPaid = loanAmount * interestRate * loanTerm;
   const payoffDate = loanTerm - payoffGoal;
@@ -20,6 +20,7 @@ function calculatePayoff() {
   `;
 
   generateAmortizationSchedule(loanAmount, interestRate, loanTerm, currentMonthlyPayment);
+  generateChart(newMonthlyPayment, currentMonthlyPayment, loanTerm);
 }
 
 function generateAmortizationSchedule(loanAmount, interestRate, loanTerm, monthlyPayment) {
@@ -44,7 +45,33 @@ function generateAmortizationSchedule(loanAmount, interestRate, loanTerm, monthl
   }
 }
 
-// Function to export data to PDF
+function generateChart(newMonthlyPayment, currentMonthlyPayment, loanTerm) {
+  const ctx = document.getElementById('payoff-chart').getContext('2d');
+  const data = {
+    labels: ['Original Payment', 'New Payment'],
+    datasets: [{
+      label: 'Monthly Payments',
+      data: [currentMonthlyPayment, newMonthlyPayment],
+      backgroundColor: ['#4ECDC4', '#00203FFF'],
+      hoverOffset: 4
+    }]
+  };
+  const config = {
+    type: 'doughnut',
+    data: data,
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      plugins: {
+        legend: {
+          position: 'top',
+        }
+      }
+    }
+  };
+  new Chart(ctx, config);
+}
+
 function exportToPDF() {
   const { jsPDF } = window.jspdf;
   const doc = new jsPDF();
